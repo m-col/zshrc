a377e15353a3f5d3ff86d63d015b506212818a92
Matt Colligan <matt.colligan@encord.com>
2025-05-15 18:08:42 +0100

    zsh fzf

--- fzf.zsh
+++ fzf.zsh
@@ -9,11 +9,22 @@ then
     #bindkey -s '^t' "fv\n"
     #export FZF_CTRL_T_COMMAND=""
 
+	#--preview 'bat -n --color=always {}'
+	#--bind 'ctrl-/:change-preview-window(down|hidden|)'
     export FZF_DEFAULT_OPTS="
-	--preview 'bat -n --color=always {}'
-	--bind 'ctrl-/:change-preview-window(down|hidden|)'
 	--tmux
 	--walker-skip .git,node_modules,target,.mypy_cache,.venv,__pycache__,.DS_Store
 	"
+    export FZF_CTRL_T_COMMAND=""
     source <(fzf --zsh)
+
+    _fzf-file-widget () {
+	[[ -z "$LBUFFER" ]] && LBUFFER="vim "
+	fzf-file-widget
+	zle accept-line
+    }
+
+    zle     -N            _fzf-file-widget
+    bindkey -M vicmd '^T' _fzf-file-widget
+    bindkey -M viins '^T' _fzf-file-widget
 fi
