51bbe8679260bfe3d416944306e86345058e1643
mcol <mcol@posteo.net>
2025-08-07 18:13:03 +0100

    git ting

--- functions.zsh
+++ functions.zsh
@@ -74,7 +74,3 @@ rec_screen() {
 rec_screen_mic() {
     ffmpeg -f x11grab -r 30 -s 1920x1080 -i $DISPLAY -f pulse -ac 2 -i default out.mkv
 }
-
-tagans() {
-    git log --pretty=format:"%an" $1...$2 | sort -u
-}
